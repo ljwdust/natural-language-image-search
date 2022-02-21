de4a7e0ae8a81c7d015d250a507760e4ea95ab5d
Jianwei Li <ljwdust@gmail.com>
2022-02-21 09:58:41 +0800

    rename variables

--- precompute_features.py
+++ precompute_features.py
@@ -1,4 +1,3 @@
-import os
 import math
 from pathlib import Path
 import numpy as np
@@ -8,74 +7,52 @@ import torch
 import clip
 
 
-dataset_version = "lite"  # Use "lite" or "full"
-model_name = "RN50x64"   # 'RN50', 'RN101', 'RN50x4', 'RN50x16', 'RN50x64', 'ViT-B/32', 'ViT-B/16', 'ViT-L/14'
-model_path = "models"
-
-# Set the path to the photos
-photos_path = Path("unsplash-dataset") / dataset_version / "photos"
-
-# Path where the feature vectors will be stored
-features_path = Path("unsplash-dataset") / dataset_version / "features" / model_name
-try:
-    os.makedirs(features_path)
-except Exception as e:
-    pass
-
-# List all JPGs in the folder
-photos_files = list(photos_path.glob("*.jpg"))
-print(f"Photos found: {len(photos_files)}")
-
+model_name = "ViT-B/16"   # 'RN50', 'RN101', 'RN50x4', 'RN50x16', 'RN50x64', 'ViT-B/32', 'ViT-B/16', 'ViT-L/14'
+root_path = r"E:\data\renminribao"
+imgdata_path = Path(root_path) / "images"
+features_path = Path(root_path) / "features" / model_name
+features_path.mkdir(parents=True, exist_ok=True)
 
 # Load the open CLIP model
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f'Running on {device}.')
+model_path = "models"
 model, preprocess = clip.load(model_name, device=device, download_root=model_path)
 
 # Function that computes the feature vectors for a batch of images
-def compute_clip_features(photos_batch):
-    # Load all the photos from the files
-    photos = [Image.open(photo_file) for photo_file in photos_batch]
-    
-    # Preprocess all photos
-    photos_preprocessed = torch.stack([preprocess(photo) for photo in photos]).to(device)
-
+def compute_clip_features(img_path_batch):
+    imgs = [Image.open(img_path) for img_path in img_path_batch]
+    imgs_preprocessed = torch.stack([preprocess(img) for img in imgs]).to(device)
     with torch.no_grad():
-        # Encode the photos batch to compute the feature vectors and normalize them
-        photos_features = model.encode_image(photos_preprocessed)
-        photos_features /= photos_features.norm(dim=-1, keepdim=True)
+        img_features = model.encode_image(imgs_preprocessed)
+        img_features /= img_features.norm(dim=-1, keepdim=True)
+    return img_features.cpu().numpy()
 
-    # Transfer the feature vectors back to the CPU and convert to numpy
-    return photos_features.cpu().numpy()
+img_files = list(imgdata_path.glob("*.jpg"))
+print(f"Images found: {len(img_files)}")
 
+batch_size = 32
+batches = math.ceil(len(img_files) / batch_size)
 
-batch_size = 16
-# Compute how many batches are needed
-batches = math.ceil(len(photos_files) / batch_size)
-
-# Process each batch
 for i in range(batches):
     print(f"Processing batch {i+1}/{batches}")
 
     batch_ids_path = features_path / f"{i:010d}.csv"
     batch_features_path = features_path / f"{i:010d}.npy"
     
-    # Only do the processing if the batch wasn't processed yet
     if not batch_features_path.exists():
         try:
-            # Select the photos for the current batch
-            batch_files = photos_files[i*batch_size : (i+1)*batch_size]
+            img_path_batch = img_files[i*batch_size : (i+1)*batch_size]
 
             # Compute the features and save to a numpy file
-            batch_features = compute_clip_features(batch_files)
+            batch_features = compute_clip_features(img_path_batch)
             np.save(batch_features_path, batch_features)
 
-            # Save the photo IDs to a CSV file
-            photo_ids = [photo_file.name.split(".")[0] for photo_file in batch_files]
-            photo_ids_data = pd.DataFrame(photo_ids, columns=['photo_id'])
-            photo_ids_data.to_csv(batch_ids_path, index=False)
+            # Save the image IDs to a CSV file
+            img_ids = [img_path.stem for img_path in img_path_batch]
+            img_ids_data = pd.DataFrame(img_ids, columns=['image_id'])
+            img_ids_data.to_csv(batch_ids_path, index=False)
         except:
-            # Catch problems with the processing to make the process more robust
             print(f'Problem with batch {i}')
 
 
@@ -86,6 +63,6 @@ features_list = [np.load(features_file) for features_file in sorted(features_pat
 features = np.concatenate(features_list)
 np.save(features_path / "features.npy", features)
 
-# Load all the photo IDs
-photo_ids = pd.concat([pd.read_csv(ids_file) for ids_file in sorted(features_path.glob("*.csv"))])
-photo_ids.to_csv(features_path / "photo_ids.csv", index=False)
+# Load all the image IDs
+img_ids = pd.concat([pd.read_csv(ids_file) for ids_file in sorted(features_path.glob("*.csv"))])
+img_ids.to_csv(features_path / "image_ids.csv", index=False)
--- util_search.py
+++ util_search.py
@@ -9,36 +9,38 @@ import ipyplot
 
 
 class CLIPSearch():
-    def __init__(self, dataset_version, model_path='models', device='cpu'):
-        # dataset_version: "lite" or "full"
-        self.dataset_version = dataset_version
+    def __init__(self, model_name, model_path='models', device='cpu'):
+        # model_name: 'RN50', 'RN101', 'RN50x4', 'RN50x16', 'RN50x64', 'ViT-B/32', 'ViT-B/16', 'ViT-L/14'
+        self.model_name = model_name
         self.model_path = model_path
         self.device = device #"cuda" if torch.cuda.is_available() else "cpu"
-        self.photos_path = Path("unsplash-dataset") / dataset_version / "photos"
+        self._load_model(model_name)
 
-    def load_model(self, model_name):
-        # model_name: 'RN50', 'RN101', 'RN50x4', 'RN50x16', 'RN50x64', 'ViT-B/32', 'ViT-B/16', 'ViT-L/14'
-        self.features_path = Path("unsplash-dataset") / self.dataset_version / "features" / model_name
+
+    def load_dataset(self, root_path='data/unsplash-dataset/lite'):
         # Load the features and the corresponding IDs
-        self.photo_features = np.load(self.features_path / "features.npy")
-        photo_ids = pd.read_csv(self.features_path / "photo_ids.csv")
-        self.photo_ids = list(photo_ids['photo_id'])
+        self.imgdata_path = Path(root_path) / "images"
+        features_path = Path(root_path) / "features" / self.model_name
+        self.img_features = np.load(features_path / "features.npy")
+        image_ids = pd.read_csv(features_path / "image_ids.csv")
+        self.image_ids = list(image_ids['image_id'])
 
-        # Load the open CLIP model
+
+    def _load_model(self, model_name):
         self.model, self.preprocess = clip.load(
             model_name, 
             device=self.device, 
             download_root=self.model_path)
 
 
-    def _encode_photo(self, photo_path):
-        photo = Image.open(photo_path)
-        photo_preprocessed = self.preprocess(photo).unsqueeze(0).to(self.device)
+    def _encode_image(self, img_path):
+        img = Image.open(img_path)
+        img_preprocessed = self.preprocess(img).unsqueeze(0).to(self.device)
         with torch.no_grad():
-            # Encode the photos batch to compute the feature vectors and normalize them
-            photos_features = self.model.encode_image(photo_preprocessed)
-            photos_features /= photos_features.norm(dim=-1, keepdim=True)
-        return photos_features #.cpu().numpy()
+            # Encode the imgs batch to compute the feature vectors and normalize them
+            img_features = self.model.encode_image(img_preprocessed)
+            img_features /= img_features.norm(dim=-1, keepdim=True)
+        return img_features #.cpu().numpy()
 
 
     def _encode_text(self, search_query):
@@ -51,31 +53,31 @@ class CLIPSearch():
 
     def _find_best_matches(self, search_features, results_count=3):
         search_features = search_features.cpu().numpy()
-        similarities = list((search_features @ self.photo_features.T).squeeze(0))
-        best_photos = sorted(zip(similarities, self.photo_ids), key=lambda x: x[0], reverse=True)
-        return best_photos[:results_count]
+        similarities = list((search_features @ self.img_features.T).squeeze(0))
+        best_imgs = sorted(zip(similarities, self.image_ids), key=lambda x: x[0], reverse=True)
+        return best_imgs[:results_count]
 
 
-    def _display_photos(self, best_photos):
-        photo_paths = [str(self.photos_path / (photo[1] + '.jpg')) for photo in best_photos]
-        ipyplot.plot_images(photo_paths, custom_texts=best_photos, img_width=300)
+    def _display_imgs(self, best_imgs):
+        img_paths = [str(self.imgdata_path / (str(img[1]) + '.jpg')) for img in best_imgs]
+        ipyplot.plot_images(img_paths, custom_texts=best_imgs, img_width=300)
 
 
     def search_by_text(self, text, results_count=3, display_in_ipython=True):
         text_feature = self._encode_text(text)
         search_results = self._find_best_matches(text_feature, results_count)
         if display_in_ipython:
-            self._display_photos(search_results)
+            self._display_imgs(search_results)
 
 
-    def search_by_photo(self, photo_path, results_count=3, display_in_ipython=True):
-        photo_feature = self._encode_photo(photo_path)
-        search_results = self._find_best_matches(photo_feature, results_count)
+    def search_by_image(self, img_path, results_count=3, display_in_ipython=True):
+        img_feature = self._encode_image(img_path)
+        search_results = self._find_best_matches(img_feature, results_count)
         if display_in_ipython:
             print('Query image:')
-            display(disImage(photo_path, width=300))
+            display(disImage(img_path, width=300))
             print('Results:')
-            self._display_photos(search_results)
+            self._display_imgs(search_results)
 
 
     def search_by_text2(self, text1, text2, weight=0.5, results_count=3, display_in_ipython=True):
@@ -85,23 +87,23 @@ class CLIPSearch():
         search_features /= search_features.norm(dim=-1, keepdim=True)
         search_results = self._find_best_matches(search_features, results_count)
         if display_in_ipython:
-            self._display_photos(search_results)
+            self._display_imgs(search_results)
 
 
-    def search_by_text_and_photo(self, photo_path, text, weight=0.5, results_count=3, display_in_ipython=True):
-        photo_feature = self._encode_photo(photo_path)
+    def search_by_text_and_image(self, img_path, text, weight=0.5, results_count=3, display_in_ipython=True):
+        img_feature = self._encode_image(img_path)
         text_feature = self._encode_text(text)
-        search_features = weight * photo_feature + (1 - weight) * text_feature
+        search_features = weight * img_feature + (1 - weight) * text_feature
         search_results = self._find_best_matches(search_features, results_count)
         if display_in_ipython:
             print('Query image:')
-            display(disImage(photo_path, width=300))
+            display(disImage(img_path, width=300))
             print('Results:')
-            self._display_photos(search_results)
+            self._display_imgs(search_results)
 
 
 if __name__ == "__main__":
     search = CLIPSearch('lite')
     search.load_model('ViT-B/32')
     search.search_by_text('five man standing under a tree')
-    # search.search_by_photo(r'.\unsplash-dataset\lite\photos\__G2yFuW7jQ.jpg')
+    # search.search_by_image(r'.\unsplash-dataset\lite\photos\__G2yFuW7jQ.jpg')
